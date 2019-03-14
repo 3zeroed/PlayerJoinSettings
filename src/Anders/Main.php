0494aa4112cb384fc4bf06ddbea00e2a6cf3823e
Anders <44516212+Anders233@users.noreply.github.com>
2019-03-14 13:33:00 +0800

    Try to send a message to the player when they join.
    Try to send a message to the player when they join.

--- src/Anders/Main.php
+++ src/Anders/Main.php
@@ -19,8 +19,8 @@ class Main extends PluginBase implements Listener{
 	
 	public function onLoad(){//Plugin Load
         	@mkdir($this->getDataFolder(),0777,true);
-        	$this->config = new Config($this->getDataFolder().'config.yml', Config::YAML);
         	$this->saveResource('config.yml', false);
+        	$this->config = new Config($this->getDataFolder().'config.yml', Config::YAML);
 	}
 	
 /*
@@ -37,15 +37,15 @@ class Main extends PluginBase implements Listener{
 		//Set the Player Join Server message to NULL.
 		//So that we can customize the player to join the join server message.
 		$event->setJoinMessage("");
-		$Title = str_replace("&", "§", strval($this->config->get("Title")))；
-		$Title = str_replace("{player}", $name, $Title);
+		$Title = str_replace("&", "§", strval($this->config->get("Title")));
+		$Title = str_replace("{name}", $name, $Title);
 		$Title = str_replace("{line}", "\n", $Title);
-		$SubTitle = str_replace("&", "§", strval($this->config->get("SubTitle")))；
-		$SubTitle = str_replace("{player}", $name, $SubTitle);
+		$SubTitle = str_replace("&", "§", strval($this->config->get("SubTitle")));
+		$SubTitle = str_replace("{name}", $name, $SubTitle);
 		$SubTitle = str_replace("{line}", "\n", $SubTitle);
 		$player->addTitle($Title, $SubTitle, 20, 60, 20);
 		$message = str_replace("&", "§", strval($this->config->get("Message")));
-		$message = str_replace("{player}", $name, $message);
+		$message = str_replace("{name}", $name, $message);
 		$message = str_replace("{line}", "\n", $message);
 		$player->sendMessage($message);
 		$PlayerJoinMessage = str_replace("&", "§", strval($this->config->get("PlayerJoinMessage")));
