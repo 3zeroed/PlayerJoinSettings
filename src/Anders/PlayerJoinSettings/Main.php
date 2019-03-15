9194b1cc8fa7efbeab4252d4c34710c0d0c9a762
Anders <44516212+Anders233@users.noreply.github.com>
2019-03-15 17:23:45 +0800

    Pop up the UI form when adding the player to join, modify the main command
    Pop up the UI form when adding the player to join, modify the main command

--- src/Anders/PlayerJoinSettings/Main.php
+++ src/Anders/PlayerJoinSettings/Main.php
@@ -6,7 +6,7 @@
  *  / ___ \ | | | || (_| ||  __/| |   \__ \
  * /_/   \_\|_| |_| \__,_| \___||_|   |___/
  * 
- * This a PocektMine-MP Plugin,There are many useful features
+ * This a PocketMine-MP Plugin,There are many useful features
  * Open source, free,If you need help, please see the contact information below.
  * GitHub:"https://github.com/Anders233",Email: fox404@Foxmail.com,
  */
@@ -17,6 +17,7 @@ namespace Anders\PlayerJoinSettings;
 
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
+use pocketmine\Player;
 use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
@@ -24,6 +25,7 @@ use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
 
 class Main extends PluginBase implements Listener{
 
@@ -51,23 +53,57 @@ class Main extends PluginBase implements Listener{
 		$name = $player->getName();
 		$online = count($this->getServer()->getOnlinePlayers());
 		$Max_online = $this->getServer()->getMaxPlayers();
-		$Title = str_replace("&", "§", strval($this->config->get("Title")));
-		$Title = str_replace("{name}", $name, $Title);
-		$Title = str_replace("{line}", "\n", $Title);
-		$SubTitle = str_replace("&", "§", strval($this->config->get("SubTitle")));
-		$SubTitle = str_replace("{name}", $name, $SubTitle);
-		$SubTitle = str_replace("{line}", "\n", $SubTitle);
-		$player->addTitle($Title, $SubTitle, 20, 60, 20);
-		$message = str_replace("&", "§", strval($this->config->get("Message")));
-		$message = str_replace("{name}", $name, $message);
-		$message = str_replace("{line}", "\n", $message);
-		$player->sendMessage($message);
-		$PlayerJoinMessage = str_replace("&", "§", strval($this->config->get("PlayerJoinMessage")));
-		$PlayerJoinMessage = str_replace("{name}", $name, $PlayerJoinMessage);
-		$PlayerJoinMessage = str_replace("{line}", "\n", $PlayerJoinMessage);
-		$PlayerJoinMessage = str_replace("{online}", $online, $PlayerJoinMessage);
-		$PlayerJoinMessage = str_replace("{max_online}", $Max_online, $PlayerJoinMessage);
-		$event->setJoinMessage($PlayerJoinMessage);
+		if($this->config->get("TitleSwitch") == "true"){//Add some switches
+			$Title = str_replace("&", "§", strval($this->config->get("Title")));
+			$Title = str_replace("{name}", $name, $Title);
+			$Title = str_replace("{line}", "\n", $Title);
+			$SubTitle = str_replace("&", "§", strval($this->config->get("SubTitle")));
+			$SubTitle = str_replace("{name}", $name, $SubTitle);
+			$SubTitle = str_replace("{line}", "\n", $SubTitle);
+			$player->addTitle($Title, $SubTitle, 20, 60, 20);
+		}
+		if($this->config->get("MessageSwitch") == "true"){//Add some switches
+			$message = str_replace("&", "§", strval($this->config->get("Message")));
+			$message = str_replace("{name}", $name, $message);
+			$message = str_replace("{line}", "\n", $message);
+			$player->sendMessage($message);
+		}
+		if($this->config->get("UndoPlayerJoinMessage") == "true"){//Add some switches
+			$event->setJoinMessage("");
+		}
+		if($this->config->get("PlayerJoinMessageSwitch") == "true"){//Add some switches
+			$PlayerJoinMessage = str_replace("&", "§", strval($this->config->get("PlayerJoinMessage")));
+			$PlayerJoinMessage = str_replace("{name}", $name, $PlayerJoinMessage);
+			$PlayerJoinMessage = str_replace("{line}", "\n", $PlayerJoinMessage);
+			$PlayerJoinMessage = str_replace("{online}", $online, $PlayerJoinMessage);
+			$PlayerJoinMessage = str_replace("{max_online}", $Max_online, $PlayerJoinMessage);
+			$this->getServer()->broadcastMessage($PlayerJoinMessage);
+		}
+		if($this->config->get("PlayerJoinUIform") == "true"){// Pop up the UI form when the player joins? (true or false)
+			$title = str_replace("&", "§", strval($this->config->get("UIformTitle")));
+			$title = str_replace("{name}", $name, $title);
+			$title = str_replace("{line}", "\n", $title);
+			$title = str_replace("{online}", $online, $title);
+			$title = str_replace("{max_online}", $Max_online, $title);
+			$content = str_replace("&", "§", strval($this->config->get("UIformContent")));
+			$content = str_replace("{name}", $name, $content);
+			$content = str_replace("{line}", "\n", $content);
+			$content = str_replace("{online}", $online, $content);
+			$content = str_replace("{max_online}", $Max_online, $content);
+			$button1 = str_replace("&", "§", strval($this->config->get("UIformbutton1")));
+			$button2 = str_replace("&", "§", strval($this->config->get("UIformbutton2")));
+			$data = [
+			  "type" => "modal",
+			  "title" => $title,
+			  "content" => $content,
+			  "button1" => $button1,
+			  "button2" => $button2
+			];
+			$packet = new ModalFormRequestPacket();
+			$packet->formId = 5072;
+			$packet->formData = json_encode($data, JSON_PRETTY_PRINT | JSON_BIGINT_AS_STRING | JSON_UNESCAPED_UNICODE);
+			$player->dataPacket($packet);
+		}
 		if($player->isOp()){//If the player is OP.
 			$player->setGamemode(1);//Set the game mode to Creative.
 		}
@@ -75,15 +111,19 @@ class Main extends PluginBase implements Listener{
 	public function onPlayerQuit(PlayerQuitEvent $event):void{
 		$player = $event->getPlayer();
 		$name = $player->getName();
-		$OnlinePlayers = count($this->getServer()->getOnlinePlayers());
-		$online = $OnlinePlayers - 1;
+		$online = count($this->getServer()->getOnlinePlayers());
 		$Max_online = $this->getServer()->getMaxPlayers();
-		$PlayerQuitMessage = str_replace("&", "§", strval($this->config->get("PlayerQuitMessage")));
-		$PlayerQuitMessage = str_replace("{name}", $name, $PlayerQuitMessage);
-		$PlayerQuitMessage = str_replace("{line}", "\n", $PlayerQuitMessage);
-		$PlayerQuitMessage = str_replace("{online}", $online, $PlayerQuitMessage);
-		$PlayerQuitMessage = str_replace("{max_online}", $Max_online, $PlayerQuitMessage);
-		$event->setQuitMessage($PlayerQuitMessage);
+		if($this->config->get("UndoPlayerQuitMessage") == "true"){//Add some switches
+			$event->setQuitMessage("");
+		}
+		if($this->config->get("PlayerQuitMessageSwitch") == "true"){//Add some switches
+			$PlayerQuitMessage = str_replace("&", "§", strval($this->config->get("PlayerQuitMessage")));
+			$PlayerQuitMessage = str_replace("{name}", $name, $PlayerQuitMessage);
+			$PlayerQuitMessage = str_replace("{line}", "\n", $PlayerQuitMessage);
+			$PlayerQuitMessage = str_replace("{online}", $online, $PlayerQuitMessage);
+			$PlayerQuitMessage = str_replace("{max_online}", $Max_online, $PlayerQuitMessage);
+			$this->getServer()->broadcastMessage($PlayerQuitMessage);
+		}
 	}
 /*
  *   ____                                                _ 
@@ -94,10 +134,18 @@ class Main extends PluginBase implements Listener{
  */
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args): bool{
 		switch($command->getName()){
-			case "reloadconfig":
-			$this->config->reload();
-			$sender->sendMessage(TextFormat::YELLOW . "Profile reloading completed!");
-			return true;
+			case "joinset":
+			if(isset($args[0])){
+				switch($args[0]){
+					case "reload":
+					$this->config->reload();
+					$sender->sendMessage(TextFormat::YELLOW . "Profile reloading completed!");
+					return true;
+				}
+			}else{
+				$sender->sendMessage(TextFormat::RED . "usage: /joinset reload");
+				return true;
+			}
 		}
 	}
 }
